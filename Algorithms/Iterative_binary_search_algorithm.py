ea96e804f2a5a67151b2f73a22025408fbf78038
Jasanjot Gill <65201683+jasanjot14@users.noreply.github.com>
2021-08-05 15:16:20 -0400

    Update Iterative_binary_search_algorithm.py

--- Algorithms/Iterative_binary_search_algorithm.py
+++ Algorithms/Iterative_binary_search_algorithm.py
@@ -1,32 +1,39 @@
 # Function to determine if a target (target) exists in a SORTED list (list) using an iterative binary search algorithm
 def iterativeBinarySearch(list, target):
+
     # Determines the search space
     first = 0
     last = len(list) - 1
 
     # Loops through the search space
     while first <= last:
+
         # Updates the midpoint in the search space
-        # first + (last - first) // 2, is used rather than, (last + first) // 2, in case of integer overflow
         midpoint = first + (last - first) // 2
-        # Returns index of target if found
+
+        # Compares target to the midpoint and returns midpoint if true
         if target == list[midpoint]:
             return midpoint
+
         # Removes all elements in the right side of the search space if target is lower than the midpoint value
         elif target < list[midpoint]:
             last = midpoint - 1
+
         # Removes all elements in the left side of the search space if target is higher than the midpoint value
         else:
             first = midpoint + 1
+
     # Returns None if target does not exist in the list
     return None
 
 
 # Function to display the results of the implemented algorithm
 def verify(index):
+
     # Notifies the user of where the target exists in the list if found
     if index is not None:
         print("The target was found at index", index, "in the list.")
+
     # Notifies the user that the target does not exist in the list
     else:
         print("The target was not found in the list.")
