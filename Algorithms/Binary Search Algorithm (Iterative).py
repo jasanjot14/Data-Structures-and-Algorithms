12b2497e30a015d26b1120559c406c23f7c1dea0
Jasanjot Gill <65201683+jasanjot14@users.noreply.github.com>
2021-08-05 12:48:32 -0400

    Update Binary Search Algorithm (Iterative).py

--- Algorithms/Binary Search Algorithm (Iterative).py
+++ Algorithms/Binary Search Algorithm (Iterative).py
@@ -22,20 +22,20 @@ def binarySearch(nums, target):
     return None
 
 
-# Function to display the results of the binary search function
+# Function to display the results of the implemented algorithm
 def verify(index):
     # Notifies the user of where the target exists in the list if found
     if index is not None:
         print("The target was found at index", index, "in the list.")
-    # Notifies the user that target does not exist in the list
+    # Notifies the user that the target does not exist in the list
     else:
         print("The target was not found in the list.")
 
 
-# Example case if target exists in the list
+# Example case for if target exists in the list
 result = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9)
 verify(result)
 
-# Example case if target does not exist in the list
+# Example case for if target does not exist in the list
 result = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 13)
 verify(result)
