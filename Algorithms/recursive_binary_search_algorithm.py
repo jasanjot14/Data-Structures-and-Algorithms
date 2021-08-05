52b2825ebf781baaec86a8ec1ed6eb23579bb91e
Jasanjot Gill <65201683+jasanjot14@users.noreply.github.com>
2021-08-05 15:19:01 -0400

    Add files via upload

---
+++ Algorithms/recursive_binary_search_algorithm.py
@@ -0,0 +1,44 @@
+# Function to determine if a target (target) exists in a SORTED list (list) using a recursive binary search algorithm
+def recursiveBinarySearch(list, target, first, last):
+
+    # Base condition
+    if first > last:
+        return None
+
+    # Updates the midpoint in the search space
+    midpoint = first + (last - first) // 2
+
+    # Compares target to midpoint and returns midpoint if true
+    if target == list[midpoint]:
+        return midpoint
+
+    # Removes all elements in the right side of the search space if target is lower than midpoint
+    elif target < list[midpoint]:
+        return recursiveBinarySearch(list, target, first, midpoint - 1)
+
+    # Removes all elements in the left side of the search space if target is higher than midpoint
+    else:
+        return recursiveBinarySearch(list, target, midpoint + 1, last)
+
+
+# Function to display the results of the implemented algorithm
+def verify(index):
+
+    # Notifies the user of where the target exists in the list if found
+    if index is not None:
+        print("The target was found at index", index, "in the list.")
+
+    # Notifies the user that the target does not exist in the list
+    else:
+        print("The target was not found in the list.")
+
+
+# Example case for if target exists in the list
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = recursiveBinarySearch(nums, 9, 0, len(nums) - 1)
+verify(result)
+
+# Example case for if target does not exist in the list
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = recursiveBinarySearch(nums, 13, 0, len(nums) - 1)
+verify(result)
